9c6ed4e79b9158aa8e3953465561c896257ec4a1
alf45tar <35426671+alf45tar@users.noreply.github.com>
2018-02-17 22:08:11 +0100

    Add files via upload

--- PedalinoESP8266.ino
+++ PedalinoESP8266.ino
@@ -5,15 +5,17 @@
 #include <WiFiUdp.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266LLMNR.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <MIDI.h>
 #include <AppleMidi.h>
 
-#define MAX_STRING  16
+#define PEDALINO_DEBUG
+
+#define WIFI_CONNECT_TIMEOUT    30
+#define SMART_CONFIG_TIMEOUT    60
 
 const char host[]     = "pedalino";
-char ssid[MAX_STRING] = "MyWireless";
-char pass[MAX_STRING] = "0000000000";
 
 ESP8266WebServer        httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
@@ -229,19 +231,143 @@ void OnAppleMidiReceiveReset(void)
 }
 
 
-void setup()
+void ap_mode_start()
 {
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  WiFi.mode(WIFI_AP);
+  boolean result = WiFi.softAP("Pedalino");
 
   MDNS.begin(host);
   httpUpdater.setup(&httpServer);
   httpServer.begin();
   MDNS.addService("http", "tcp", 80);
+}
 
+
+bool smart_config()
+{
+  // Return 'true' if SSID and password received within SMART_CONFIG_TIMEOUT seconds
+
+  // Re-establish lost connection to the AP
+  WiFi.setAutoReconnect(true);
+
+  // Automatically connect on power on to the last used access point
+  WiFi.setAutoConnect(true);
+
+  // Waiting for SSID and password from from mobile app
+  // SmartConfig works only in STA mode
+  WiFi.mode(WIFI_STA);
+  WiFi.beginSmartConfig();
+
+#ifdef PEDALINO_DEBUG
+  Serial.println("");
+  Serial.print("SmartConfig started");
+#endif
+
+  for (int i = 0; i < SMART_CONFIG_TIMEOUT * 2 && !WiFi.smartConfigDone(); i++) {
+    delay(500);
+#ifdef PEDALINO_DEBUG
+    Serial.print(".");
+#endif
+  }
+
+#ifdef PEDALINO_DEBUG
+  if (WiFi.smartConfigDone())
+  {
+    Serial.println("[SUCCESS]");
+    Serial.printf("SSID        : %s\n", WiFi.SSID().c_str());
+    Serial.printf("Password    : %s\n", WiFi.psk().c_str());
+  }
+  else
+    Serial.println("[NO SUCCESS]");
+#endif
+
+  WiFi.stopSmartConfig();
+
+  return WiFi.stopSmartConfig();
+}
+
+bool auto_reconnect()
+{
+  // Return 'true' if connected to the (last used) access point within WIFI_CONNECT_TIMEOUT seconds
+
+#ifdef PEDALINO_DEBUG
+  Serial.println("");
+  Serial.printf("Connecting to %s ", WiFi.SSID().c_str());
+#endif
+
+  for (byte i = 0; i < WIFI_CONNECT_TIMEOUT * 2 && WiFi.status() != WL_CONNECTED; i++) {
+    delay(500);
+#ifdef PEDALINO_DEBUG
+    Serial.print(".");
+#endif
+  }
+
+#ifdef PEDALINO_DEBUG
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("[SUCCESS]");
+  else
+    Serial.println("[NO SUCCESS]");
+#endif
+
+  return WiFi.status() == WL_CONNECTED;
+}
+
+void wifi_connect()
+{
+  if (!auto_reconnect())       // WIFI_CONNECT_TIMEOUT seconds to reconnect to last used access point
+    if (smart_config())        // SMART_CONFIG_TIMEOUT seconds to receive SmartConfig parameters
+      auto_reconnect();        // WIFI_CONNECT_TIMEOUT seconds to connect to SmartConfig access point
+  if (WiFi.status() != WL_CONNECTED)
+    ap_mode_start();          // switch to AP mode until next reboot
+  else
+  {
+    WiFi.hostname(host);
+
+#ifdef PEDALINO_DEBUG
+    Serial.println("");
+    WiFi.printDiag(Serial);
+    Serial.println("");
+
+    uint8_t macAddr[6];
+    WiFi.macAddress(macAddr);
+    Serial.printf("BSSID       : %s\n", WiFi.BSSIDstr().c_str());
+    Serial.printf("RSSI        : %d dBm\n", WiFi.RSSI());
+    Serial.printf("Hostname    : %s\n", WiFi.hostname().c_str());
+    Serial.printf("STA         : %02X:%02X:%02X:%02X:%02X:%02X\n", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+    Serial.printf("IP address  : %s\n", WiFi.localIP().toString().c_str());
+    Serial.printf("Subnet mask : %s\n", WiFi.subnetMask().toString().c_str());
+    Serial.printf("Gataway IP  : %s\n", WiFi.gatewayIP().toString().c_str());
+    Serial.printf("DNS 1       : %s\n", WiFi.dnsIP(0).toString().c_str());
+    Serial.printf("DNS 2       : %s\n", WiFi.dnsIP(1).toString().c_str());
+    Serial.println("");
+#endif
+    // Start LLMNR (Link-Local Multicast Name Resolution) responder
+    LLMNR.begin(host);
+#ifdef PEDALINO_DEBUG
+    Serial.println("LLMNR responder started");
+#endif
+
+    // Start mDNS (Multicast DNS) responder
+    if (MDNS.begin(host, WiFi.localIP())) {
+#ifdef PEDALINO_DEBUG
+      Serial.println("mDNS responder started");
+#endif
+      MDNS.addService("apple-midi", "udp", 5004);
+    }
+    // Start firmawre update via HTTP (connect to http://pedalino/update)
+    httpUpdater.setup(&httpServer);
+    httpServer.begin();
+    MDNS.addService("http", "tcp", 80);
+#ifdef PEDALINO_DEBUG
+    Serial.println("HTTP server started");
+    Serial.println("Connect to http://pedalino/update for firmware update");
+#endif
+  }
+
+}
+
+void midi_connect()
+{
   // Connect the handle function called upon reception of a MIDI message from serial MIDI interface
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
@@ -292,6 +418,23 @@ void setup()
   AppleMIDI.OnReceiveReset(OnAppleMidiReceiveReset);
 }
 
+void setup()
+{
+#ifdef PEDALINO_DEBUG
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("");
+  Serial.println("*** Pedalino(TM) ***");
+#endif
+
+  wifi_connect();
+
+#ifdef PEDALINO_DEBUG
+  Serial.flush();
+#endif
+
+  midi_connect();
+}
 
 void loop()
 {
