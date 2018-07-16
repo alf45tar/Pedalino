e7fa65f110f352365165d932bedf7c8ef7c82462
alf45tar <35426671+alf45tar@users.noreply.github.com>
2018-07-16 22:45:13 +0200

    Add files via upload

--- PedalinoESP/PedalinoESP.ino
+++ PedalinoESP/PedalinoESP.ino
@@ -13,6 +13,20 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define MIDI_SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
+#define MIDI_CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
+
+BLEServer           *pServer;
+BLEService          *pService;
+BLECharacteristic   *pCharacteristic;
+BLESecurity         *pSecurity;
+bool                bleMidiConnected = false;
 #endif
 
 #include <WiFiClient.h>
@@ -23,6 +37,7 @@
 #include <OSCMessage.h>
 #include <OSCBundle.h>
 #include <OSCData.h>
+
 
 #define PEDALINO_SERIAL_DEBUG
 //#define PEDALINO_TELNET_DEBUG
@@ -46,8 +61,15 @@ RemoteDebug Debug;
 #define SERIALDEBUG       Serial
 #endif
 
+#ifdef ARDUINO_ARCH_ESP8266
 #define BUILTIN_LED_OFF() digitalWrite(BUILTIN_LED, HIGH)
 #define BUILTIN_LED_ON()  digitalWrite(BUILTIN_LED, LOW)
+#endif
+
+#ifdef ARDUINO_ARCH_ESP32
+#define BUILTIN_LED_OFF() digitalWrite(BUILTIN_LED, LOW)
+#define BUILTIN_LED_ON()  digitalWrite(BUILTIN_LED, HIGH)
+#endif
 
 const char host[]     = "pedalino";
 
@@ -698,6 +720,58 @@ void wifi_connect()
 #endif
 }
 
+class BLECallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      bleMidiConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      bleMidiConnected = false;
+    }
+};
+
+void ble_connect ()
+{
+#ifdef ARDUINO_ARCH_ESP32
+
+  BLEDevice::init("Pedal");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new BLECallbacks());
+  BLEDevice::setEncryptionLevel((esp_ble_sec_act_t)ESP_LE_AUTH_REQ_SC_BOND);
+
+  // Create the BLE Service
+  pService = pServer->createService(BLEUUID(MIDI_SERVICE_UUID));
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      BLEUUID(MIDI_CHARACTERISTIC_UUID),
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_WRITE_NR
+                    );
+  pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pSecurity = new BLESecurity();
+  pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_BOND);
+  pSecurity->setCapability(ESP_IO_CAP_NONE);
+  pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+
+  pServer->getAdvertising()->addServiceUUID(MIDI_SERVICE_UUID);
+  pServer->getAdvertising()->start();
+
+#endif
+}
+
 void midi_connect()
 {
   // Connect the handle function called upon reception of a MIDI message from serial MIDI interface
@@ -770,6 +844,8 @@ void setup()
   SERIALDEBUG.println("*** Pedalino(TM) ***");
 #endif
 
+  ble_connect();
+
   // Write SSID/password to flash only if currently used values do not match what is already stored in flash
   WiFi.persistent(false);
   wifi_connect();
@@ -788,7 +864,8 @@ void setup()
 
 void loop()
 {
-  if (appleMidiConnected) {
+  if (!appleMidiConnected && !bleMidiConnected) BUILTIN_LED_OFF();
+  if (appleMidiConnected || bleMidiConnected) {
     // led fast blinking (5 times per second)
     if (millis() - lastOn > 200) {
       BUILTIN_LED_ON();
